bb78a724491046f3453c02d6d004831a8408ef94
Michael Germany <mike@rickgermany.com>
2015-03-12 00:27:45 +0000

    commit 4-2

--- microscope.js
+++ microscope.js
@@ -18,6 +18,23 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    
     // code to run on server at startup
+    if (Posts.find().count() === 0) {
+      Posts.insert({
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+      });
+    
+      Posts.insert({
+        title: 'Meteor',
+        url: 'http://meteor.com'
+      });
+    
+      Posts.insert({
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+      });
+    }
   });
 }
